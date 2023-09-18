ddd0f07235f58f041ff5bdf46d4efb7a8019404d
godfatherzaher <143941932+godfatherzaher@users.noreply.github.com>
2023-09-19 02:39:10 +0300

    commit all files

---
+++ src/components/ChannelDetail.jsx
@@ -0,0 +1,55 @@
+import {useState,useEffect} from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material"; 
+
+import {Videos,ChannelCard} from './';
+import { fetchFromAPIe } from "../utils/fetchFromAPIe";
+
+
+const ChannelDetail = () => {
+    const [channelDetail,setChannelDetail] = useState()
+    const [videos,setVideos]= useState([])
+
+    const {id} = useParams();
+
+    
+
+
+    useEffect(()=>{
+        const fetchResults = async () => {
+            const data = await fetchFromAPIe(`channels?part=snippet&id=${id}`);
+      
+            setChannelDetail(data?.items[0]);
+      
+            const videosData = await fetchFromAPIe(`search?channelId=${id}&part=snippet%2Cid&order=date`);
+      
+            setVideos(videosData?.items);
+          };
+      
+          fetchResults();
+        }, [id]);
+
+
+
+    return (
+        <Box minHeight='95vh'>
+            <Box>
+                <div style={{
+                    background:'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
+                    zIndex:10,
+                    height:'300px'
+                }}/>
+                {channelDetail?.snippet?.country&&
+                (<ChannelCard channelDetail={channelDetail} marginTop="-110px"/>)
+                }
+            <Box display='flex' p='2'>
+                <Box sx={{mr:{sm:'120px'}}}/>
+                <Videos videos={videos}/>
+     
+            </Box>
+            </Box>
+        </Box>
+    )
+}
+
+export default ChannelDetail
---
+++ src/components/Feed.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+import { Box, Stack, Typography } from "@mui/material";
+import { Videos, Sidebar } from "./"
+import { fetchFromAPIe } from "../utils/fetchFromAPIe";
+
+
+
+
+
+const Feed = () => {
+    const [selectedCategory, setselectedCategory] = useState("New")
+    const [videos, setVideos] = useState([])
+
+
+
+    useEffect(() => {
+        fetchFromAPIe(`search?part=snippet&q=${selectedCategory}`)
+            .then((data) =>  setVideos(data.items) )
+    }, [selectedCategory])
+
+    return (
+        <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+            <Box sx={{
+                height: { sx: "auto", md: "92vh" },
+                borderRight: "1px solid #3d3d3d",
+                px: { sx: 0, md: 2 }
+            }}>
+                <Sidebar
+                    selectedCategory={selectedCategory}
+                    setselectedCategory={setselectedCategory}
+                />
+
+                <Typography className="copyright"
+                    varint="body2"
+                    sx={{ mt: 1.5, color: "#fff" }}>
+                    Copyright 2023 Hasan Zaher
+                </Typography>
+            </Box>
+
+            <Box p={2} sx={{
+                overflowY: "auto",
+                height: "90vh",
+                flex: 2
+            }}>
+                <Typography variant="h4"
+                    fontWeight="bold"
+                    mb={2} sx={{
+                        color: "white"
+                    }}>
+                    {selectedCategory}
+                    <span style={{ color: "#F31503" }}>Videos</span>
+                </Typography>
+                <Videos videos={videos} />
+            </Box>
+
+        </Stack>
+    )
+}
+
+export default Feed
